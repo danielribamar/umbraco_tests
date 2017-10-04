72c1f9f1c935eaada648407440c917187a92b129
Daniel Ribamar Martins <daniel.ribamarmartins@hotmail.com>
2017-10-04 17:13:44 +0100

    ADD: files

---
+++ Umbraco.Tests/App_Plugins/FontAwesomePicker/fontawesomepicker.controller.js
@@ -0,0 +1,15 @@
+﻿angular.module("umbraco")
+    .controller("Custom.FontawesomePicker",
+    function ($scope, assetsService, dialogService) {
+        console.log($scope.model.value);
+        assetsService
+            .load([
+            ])
+            .then(function () {
+                console.log("::fontawesome picker:: dependencies loaded");
+            });
+        $.getJSON('../App_Plugins/FontAwesomePicker/data/icons.json', function (json) {
+            console.log('loading icons');
+            $scope.icons = json;
+        });
+    });
